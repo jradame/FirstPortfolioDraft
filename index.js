303a95264f8d19091eb53b81aef715b812db4841
justin <jradame@gmail.com>
2025-05-26 14:59:30 -0500

    advancedproject

---
+++ index.js
@@ -0,0 +1,23 @@
+// template_zns0lbf
+// service_sv36fer
+// HTqTsoxXu_8N9-M5H
+
+function contact(event) {
+	event.preventDefault();
+// 	emailjs
+// 	.sendform(
+// 		'service_sv36fer',
+// 		'template_zns0lbf',
+// 		event.target,
+// 		'user_HTqTsoxXu_8N9-M5H'
+// 	).then(() => {
+// 		 console.log('it worked')
+
+// 	})
+const loading = document.querySelector('.modal__overlay--loading');
+const success = document.querySelector('.modal__overlay--success');
+loading.classList += " modal__overlay--visible";
+setTimeout(() => {
+	console.log('it worked 1')
+}, 500);
+}	
