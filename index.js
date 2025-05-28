ba76d8207d6872d7c34da9c9cd5fce3aeabab31f
justin <jradame@gmail.com>
2025-05-27 19:37:14 -0500

    advancedproject

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-// template_zns0lbf
-// service_sv36fer
-// HTqTsoxXu_8N9-M5H
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor; 
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const oddInteger = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * oddInteger}px, ${y * oddInteger}px)`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList.add("dark-theme");
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
 
 function contact(event) {
   event.preventDefault();
@@ -27,15 +48,12 @@ function contact(event) {
     });
 }
 
-let isModalOpen = false;
 function toggleModal() {
+  isModalOpen = !isModalOpen;
   if (isModalOpen) {
-    isModalOpen = false;
-    return document.body.classList.remove("modal--open");
-  } 
-  else {
-    isModalOpen = true;
-    document.body.classList += " modal--open";
+    document.body.classList.add("modal--open");
+  } else {
+    document.body.classList.remove("modal--open");
   }
 }
 
